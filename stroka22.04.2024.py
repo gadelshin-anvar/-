edf0f3b00f4f1be17cfaa447e870927b4fe07351
gadelshin-anvar <167808387+gadelshin-anvar@users.noreply.github.com>
2024-04-23 00:12:56 +0300

    Add files via upload

---
+++ stroka22.04.2024.py
@@ -0,0 +1,8 @@
+name = "это моя строка"
+print(name * 6)
+print(name[0])
+print(name[-1])
+print(name[2:6])
+print(name[::-1])
+print(len('это моя строка')) # => 14 это точно, полчаса ломал голову
+print('это новая строка ' + name)
