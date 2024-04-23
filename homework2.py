7f9623fd41f5ce3b52a1a54354f4a99da700dfc7
gadelshin-anvar <167808387+gadelshin-anvar@users.noreply.github.com>
2024-04-23 15:31:41 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,12 @@
+name = ('Anvar') # => str
+print ('Anvar')
+print (type (name))
+age = (42) # => int
+print (42)
+print (type (age))
+NewAge = (age * 2 - 32) # => int, = 52
+print (type (NewAge))
+print (NewAge) # = 52
+is_student = (42 * 2 - 32 == 52)
+print (type (is_student)) # => boolean
+print(is_student) # => True
