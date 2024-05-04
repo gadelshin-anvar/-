92d17965b522b2a52db5194e537608856cdb11f6
gadelshin-anvar <167808387+gadelshin-anvar@users.noreply.github.com>
2024-05-04 10:24:48 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,7 @@
+name = input('Укажите Вашу должность: ')
+my_string = str(name)
+print (len(my_string))
+print (str(my_string).upper())
+print (str(my_string).lower())
+print (my_string [0])
+print (my_string [-1])
